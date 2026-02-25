c5da416a11d9ff5c548fc1cf396486e96c673947
ynbh <yashasclg@gmail.com>
2026-02-25 02:03:58 -0500

    chore: remove TypeScript OpenAPI config

--- openapi-ts.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CreateClientConfig } from "@hey-api/client-fetch";
-
-console.log(process.env.CANVAS_API_TOKEN);
-
-export const createClientConfig: CreateClientConfig = (config) => ({
-  ...config,
-  auth: () => `${process.env.CANVAS_API_TOKEN ?? ""}`,
-  baseURL: "https://canvas.instructure.com/api"
-});
